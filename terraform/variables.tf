c1ad220fa46eac90b2a12f3bbcbb11a31eb525dd
Prasad Naidu <36126926+PrasadNaidu1082@users.noreply.github.com>
2025-03-15 18:32:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,28 @@
 
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "bucket_name" {
+  default = "drug-discovery-tf-bucket"
+}
+
+variable "dynamodb_table_name" {
+  default = "DrugDiscoveryTFTable"
+}
+
+variable "lambda_role_name" {
+  default = "DrugDiscoveryLambdaRole"
+}
+
+variable "upload_lambda_name" {
+  default = "UploadCSVLambdaTF"
+}
+
+variable "retrieve_lambda_name" {
+  default = "RetrieveDataLambdaTF"
+}
+
+variable "api_gateway_name" {
+  default = "DrugDiscoveryAPITF"
+}

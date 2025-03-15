bfd18d2870f8ae5a2f831e8cadd3e3fb778882a4
Prasad Naidu <36126926+PrasadNaidu1082@users.noreply.github.com>
2025-03-15 18:36:52 +0530

    Update retrieve_data_lambda.py

--- lambdas/retrieve_data_lambda.py
+++ lambdas/retrieve_data_lambda.py
@@ -1,1 +1,26 @@
+import boto3
+import json
+import os
+
+dynamodb = boto3.resource("dynamodb")
+
+# Environment variables
+TABLE_NAME = os.environ["TABLE_NAME"]
+
+def lambda_handler(event, context):
+    try:
+        # Query DynamoDB for all data
+        table = dynamodb.Table(TABLE_NAME)
+        response = table.scan()
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps(response.get("Items", []))
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
 

c3d2db561e5f3d41af1168114901feb7b4f94fc4
Prasad Naidu <36126926+PrasadNaidu1082@users.noreply.github.com>
2025-03-15 18:33:26 +0530

    Update main.tf

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -1,1 +1,16 @@
+resource "aws_dynamodb_table" "drug_discovery" {
+  name           = var.dynamodb_table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "DrugID"
+
+  attribute {
+    name = "DrugID"
+    type = "S"
+  }
+}
+
+variable "dynamodb_table_name" {}
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.drug_discovery.name
+}
 

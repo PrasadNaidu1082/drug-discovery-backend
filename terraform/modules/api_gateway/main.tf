e66457f1b1ea724f1537f50bc1ef3a3da4aae219
Prasad Naidu <36126926+PrasadNaidu1082@users.noreply.github.com>
2025-03-16 09:08:07 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -29,10 +29,84 @@ resource "aws_api_gateway_method" "retrieve_get" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_integration" "upload_post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.upload_resource.id
+  http_method             = aws_api_gateway_method.upload_post.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = var.upload_lambda_arn
+
+  request_templates = {
+    "text/csv" = <<EOF
+{
+  "body": "$util.base64Encode($input.body)"
+}
+EOF
+  }
+}
+
+resource "aws_api_gateway_integration" "retrieve_get_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.retrieve_resource.id
+  http_method             = aws_api_gateway_method.retrieve_get.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = var.retrieve_lambda_arn
+}
+
+resource "aws_api_gateway_method_response" "upload_post_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.upload_resource.id
+  http_method = aws_api_gateway_method.upload_post.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_method_response" "retrieve_get_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.retrieve_resource.id
+  http_method = aws_api_gateway_method.retrieve_get.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "upload_post_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.upload_resource.id
+  http_method = aws_api_gateway_method.upload_post.http_method
+  status_code = "200"
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+resource "aws_api_gateway_integration_response" "retrieve_get_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.retrieve_resource.id
+  http_method = aws_api_gateway_method.retrieve_get.http_method
+  status_code = "200"
+  response_templates = {
+    "application/json" = ""
+  }
+}
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.upload_post_integration,
+    aws_api_gateway_integration.retrieve_get_integration
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name = "prod"
+}
+
+resource "aws_api_gateway_stage" "prod_stage" {
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "prod"
+}
+
 variable "upload_lambda_arn" {}
 variable "retrieve_lambda_arn" {}
 
 output "api_url" {
-  value = aws_api_gateway_rest_api.api.execution_arn
+  value = aws_api_gateway_stage.prod_stage.invoke_url
 }
-
